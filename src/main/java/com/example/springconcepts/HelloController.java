c1df86e8e7171cd07511268a598f6045109136ae
ayush0815 <predatorof09@gmail.com>
2025-03-01 11:18:50 +0530

    param name

--- src/main/java/com/example/springconcepts/HelloController.java
+++ src/main/java/com/example/springconcepts/HelloController.java
@@ -1,7 +1,7 @@
 package com.example.springconcepts;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")  // Base URL path for endpoints
 public class HelloController {
 
-    // Query Parameter API: localhost:8080/hello/query?name=Mark
-    @GetMapping("/query")
-    public String sayHello(@RequestParam String name) {
+    // Path Variable API: localhost:8080/hello/param/Mark
+    @GetMapping("/param/{name}")
+    public String sayHello(@PathVariable String name) {
         return "Hello " + name + " from BridgeLabz!";
     }
 }
