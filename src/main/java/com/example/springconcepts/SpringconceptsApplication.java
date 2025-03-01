a257529b260a9a8014d73b7a3d55ea576f3e2d3e
ayush0815 <predatorof09@gmail.com>
2025-03-01 11:09:15 +0530

    Hello bridgelabz

---
+++ src/main/java/com/example/springconcepts/SpringconceptsApplication.java
@@ -0,0 +1,12 @@
+package com.example.springconcepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class SpringConceptsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConceptsApplication.class, args);
+		System.out.println("Spring Application Started!");
+	}
+}
